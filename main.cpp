0ec6f18d498ec7adb96bb3b021660a0ee82cbc25
BDZNH <pxezhsp@live.com>
2018-04-21 01:18:58 +0800

    Fix a Big Bug,(ง •_•)ง

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
-#include<fstream>
-#include<unistd.h>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
 #include <wiringPi.h>
 #include <softPwm.h>
 #define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
+#define LOG_PATH "/tmp/fanspped.log"
 #define _FANPIN 8
 using namespace std;
 
@@ -14,8 +15,13 @@ void showInfo();
 int main()
 {
 	showInfo();
+	ofstream log(LOG_PATH,ios::out | ios::app);
+	if(!log.is_open())
+	{
+		cout<<"Cant open file :"<<LOG_PATH<<endl;
+	}
 	double temp = 0;
-	bool Fan_is_open = false;
+	bool Fifty_Flag = false;
 	if (initWiringPi() < 0)
 	{
 		return -1;
@@ -24,54 +30,71 @@ int main()
 	{
 		temp = GetCpuTempera();
 		if (temp >= 42 )
-			cout << "Cpu temperature is : \033[0;31m" <<temp << flush << "°C \033[0m";
+			cout << "Cpu temperature is : \033[0;31m" <<temp <<"°C \033[0m";
 		else
-			cout << "Cpu temperature is : \033[1;32m" <<temp << flush << "°C \033[0m";
-		if (Fan_is_open)
+			cout << "Cpu temperature is : \033[1;32m" <<temp <<"°C \033[0m";
+		if (Fifty_Flag)
 		{
 			if (temp < 39.0)
 			{
-				Fan_is_open = false;
-				sleep(10);
+				Fifty_Flag = false;
+				delay(1000);
 				softPwmWrite(_FANPIN, 0);
 			}
-			sleep(5);
+			else
+			{
+				delay(1000);
+			}
 		}
 		else
-		{	
-			if (temp >40 && temp < 41)
+		{				
+			if (temp < 39.0)
 			{
-				Fan_is_open = true;
+				delay(1000);
+				softPwmWrite(_FANPIN, 0);
+			}
+			if (temp >40.0 && temp < 41.0)
+			{
 				softPwmWrite(_FANPIN, 60);
-				//cout << "set fan speed 60"<<flush;
+				//log << "  set fan speed 60"<<endl;
 			}
-			if (temp >=41 && temp < 42)
+			else if (temp >=41.0 && temp < 42.0)
 			{
-				Fan_is_open = true;
 				softPwmWrite(_FANPIN, 70);
-				//cout << "set fan speed 70"<<flush;
+				//log << "  set fan speed 70"<<endl;
+				//cout << "speed 70" <<flush;
+				//log.clear();
+				//log.seekg(0, ios::beg);
 			}
-			if (temp >=42 && temp < 43)
+			else if (temp >=42.0 && temp < 43.0)
 			{
-				Fan_is_open = true;
 				softPwmWrite(_FANPIN, 80);
-				//cout << "set fan speed 80"<<flush;
+				//log << "  set fan speed 80"<<endl;
+				//cout << " speed 80 " <<flush;
+				//log.clear();
+				//log.seekg(0, ios::beg);
 			}
-			if (temp >=44 && temp < 45)
+			else if (temp >=43.0 && temp < 45.0)
 			{
-				Fan_is_open = true;
 				softPwmWrite(_FANPIN, 90);
-				//cout << "set fan speed 90"<<flush;
+				//log << "  set fan speed 90"<<endl;
+				//cout << "Speed 90" <<flush;
+				//log.clear();
+				//log.seekg(0, ios::beg);
 			}
-			if (temp > 45.0)
+			else if (temp > 45.0)
 			{
-				Fan_is_open = true;
 				softPwmWrite(_FANPIN, 100);
-				//cout << "set fan speed 100"<<flush;
-				sleep(60);
+				//log << "  set fan speed 100"<<endl;
+				//cout << "Speed 100" <<flush;
+				Fifty_Flag = true ;
+				//log.clear();
+				//log.seekg(0, ios::beg);
+				delay(5000);
 			}
 		}
-		cout<<"\r";
+		delay(2000);
+		cout<<flush<<"\r";
 	}
 	return 0;
 }
